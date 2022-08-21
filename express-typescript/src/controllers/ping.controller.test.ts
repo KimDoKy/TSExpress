cffdb522c44ad18e5b9b623a4e51bb73bebbea3c
DoKyung Kim <dokyungkim@DoKyungs-MacBook-Air.local>
2022-08-21 15:25:28 +0900

    Setup Jest

---
+++ express-typescript/src/controllers/ping.controller.test.ts
@@ -0,0 +1,7 @@
+import PingController from "./ping.controller";
+
+test("it should pass", async () => {
+    const controller = new PingController();
+    const response = await controller.getMessage();
+    expect(response.message).toBe("pong");
+});
