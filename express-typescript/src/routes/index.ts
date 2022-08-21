a76d76aa437481781bf6dc9a37e8c608cdba7335
DoKyung Kim <dokyungkim@DoKyungs-MacBook-Air.local>
2022-08-21 14:24:11 +0900

    Setup Jest

--- express-typescript/src/routes/index.ts
+++ express-typescript/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import PingController from "../controllers/ping";
+import PingController from "../controllers/ping.controller";
 import UserRouter from "./user.router";
 import PostRouter from "./post.router";
 import CommentRouter from "./comment.router";
